e240e4035d0dc4fbde8857ae7c19077f4de0d78e
yaelsarusi <yaelsarusi@gmail.com>
2023-08-05 18:29:12 +0300

    refactor most doc string

--- count_assignments.py
+++ count_assignments.py
@@ -1,5 +1,5 @@
 from enum import Enum, IntEnum
-from typing import List, Optional
+from typing import List, Optional, Tuple, Union
 from dataclasses import dataclass
 import math
 
@@ -10,6 +10,8 @@ class Floor(IntEnum):
     Fourth = 4
     Fifth = 5
 
+MIN_FLOOR = min(floor.value for floor in Floor)
+MAX_FLOOR = max(floor.value for floor in Floor)
 
 class Color(Enum):
     Red = 'Red'
@@ -35,23 +37,45 @@ class AttributeType(Enum):
 @dataclass
 class FloorAssignment:
     floor: Floor
-    color: Optional[Color]
-    animal: Optional[Animal]
+    color: Color
+    animal: Animal
 
-    def __contains__(self, value):
+    def __contains__(self, value: Union[Floor, Color, Animal]):
         return value == self.floor or value == self.color or self.animal == value
 
 class Hint(object): 
     """Base class for all the hint classes"""
 
-    def check_if_satisfied(self, floor_assignments):
-        """ Hello documentaion """
+    def check_if_satisfied(self, floor_assignments: List[FloorAssignment]) -> bool:
+        """ Checks if the hint is satisfied in the given floor assignments. """
         ...
 
-    def get_possible_floor_assignments(self, empty_floors, all_animal_options, all_color_options, floor_assignments):
+    def get_possible_floor_assignments(self, 
+                                       empty_floors: List[Floor], 
+                                       all_animal_options: List[Animal], 
+                                       all_color_options: List[Color], 
+                                       floor_assignments: List[FloorAssignment]) -> List[Tuple[Optional[FloorAssignment], Optional['Hint']]]:
         """
+        Returns a list of all posssible tuples, of floor assignments and additional hints.
+        The combination of both will satisfy the original hint to completion.
         """
         ...
+
+
+def find_floor_assignment_by_attribute(attribute: Union[Floor, Color, Animal], floor_assignments: List[FloorAssignment]) -> Optional[FloorAssignment]:
+    floor_assignment_with_attribute = list(filter(lambda x: attribute in x, floor_assignments))
+    if len(floor_assignment_with_attribute) == 1:
+        return floor_assignment_with_attribute[0]
+    return None
+
+def remove_value(lst, value):
+    return list(filter(lambda x: x != value, lst))
+
+@dataclass
+class GetOptionsIfValidReturnType:
+    is_valid: bool
+    new_options: List[Union[Color, Animal, Floor]]
+    floor_assignment: Union[List[Color], List[Animal], List[Floor]]
 
 class RelativeHint(Hint):
     """
@@ -65,46 +89,78 @@ class RelativeHint(Hint):
     The third floor is two floors below the fifth floor:
         RelativeHint(Floor.Third, Floor.Fifth, -2)
     """
-    def __init__(self, attr1, attr2, difference): # Reminder: Don't change the initializer signature
+    def __init__(self, 
+                 attr1: Union[Floor, Color, Animal], 
+                 attr2: Union[Floor, Color, Animal],
+                 difference: int): # Reminder: Don't change the initializer signature
         self._attr1 = attr1
         self._attr2 = attr2
         self._difference = difference
-
     
-    def check_if_satisfied(self, floor_assignments):
-        floor_assignment_with_attr1 = list(filter(lambda x: self._attr1 in x, floor_assignments))
-        floor_assignment_with_attr2 = list(filter(lambda x: self._attr2 in x, floor_assignments))
+    def check_if_satisfied(self, floor_assignments: List[FloorAssignment]):
+        """
+        Checks if the hint is satisfied in the given floor assignments. 
+        A Relative hint is satisfied if in the given floor assignment lists, exists 2 floor assignments that satisfy:
+        * Both have one of the respective hint attributes.
+        * Their floor distance is the hint given difference.
+        """
+        floor_assignment_with_attr1 = find_floor_assignment_by_attribute(self._attr1, floor_assignments)
+        floor_assignment_with_attr2 = find_floor_assignment_by_attribute(self._attr2, floor_assignments)
 
-        if len(floor_assignment_with_attr1) == 1 and len(floor_assignment_with_attr2) == 1:
-            # both attributes were assigned to floors,
-            # check if difference is correct
-            new_floor = floor_assignment_with_attr2[0].floor.value + self._difference
-            if new_floor < 6 and new_floor > 0:
-                return Floor(new_floor) in floor_assignment_with_attr1[0]
+        # Checks if both attributes were assigned to floors
+        if floor_assignment_with_attr1 is not None and floor_assignment_with_attr2 is not None:
+            new_floor = floor_assignment_with_attr2.floor.value + self._difference
+            # check if floor difference is correct
+            if new_floor <= MAX_FLOOR and new_floor >= MIN_FLOOR:
+                return Floor(new_floor) in floor_assignment_with_attr1
 
         return False
     
-    def get_possible_floor_assignments(self, empty_floors, all_animal_options, all_color_options, floor_assignments):
+    def get_options_if_valid(self, options: Union[List[Color], List[Animal], List[Floor]], floor_assignments: List[FloorAssignment]) -> GetOptionsIfValidReturnType:
+        if self._attr1 in options:
+            return GetOptionsIfValidReturnType(True, [self._attr1], [])
+        elif self._attr2 in options:
+            floor_assignment_with_attr1 = find_floor_assignment_by_attribute(self._attr1, floor_assignments)
+
+            new_floor = floor_assignment_with_attr1.floor.value - self._difference
+            if new_floor <= MAX_FLOOR and new_floor >= MIN_FLOOR:
+                return GetOptionsIfValidReturnType(False, [], [(None, [AbsoluteHint(Floor(new_floor), self._attr2 )])])
+            return GetOptionsIfValidReturnType(False, [], [])
+        else:
+            return GetOptionsIfValidReturnType(False, [], [])
+    
+    def get_possible_floor_assignments(self, 
+                                       empty_floors: List[Floor], 
+                                       all_animal_options: List[Animal], 
+                                       all_color_options: List[Color], 
+                                       floor_assignments: List[FloorAssignment]) -> List[Tuple[Optional[FloorAssignment], Optional['Hint']]]:
+        """
+        A possible floor assignment of a relative hint:
+        1) A floor assignment to satisfy the first attribute of the hint if it's not already in the floor assignments given.
+        2) An Absolute hint that links between the existing (or new) floor assignment that thas the first attribute, and a future floor assignmnet of the second attribute.
+        """
+        
+        # get_options_if_valid = self.get_options_if_valid(all_animal_options, floor_assignments)
+        # if not get_options_if_valid.is_valid:
+        #     return get_options_if_valid.floor_assignment
+        # else:
+        #     animal_options = get_options_if_valid.new_options
+        
         animal_options = all_animal_options
         color_options = all_color_options
         floor_options = empty_floors
+
+        if isinstance(self._attr1, Animal):
+            get_options_if_valid = self.get_options_if_valid(all_animal_options, floor_assignments)
+            if not get_options_if_valid.is_valid:
+                return get_options_if_valid.floor_assignment
+            else:
+                animal_options = get_options_if_valid.new_options
 
         if isinstance(self._attr1, Floor):
             if self._attr1 in empty_floors:
                 floor_options = [self._attr1]
             elif self._attr2 in empty_floors:
-                floor_assignment_with_attr1 = list(filter(lambda x: self._attr1 in x, floor_assignments))
-                new_floor = floor_assignment_with_attr1[0].floor.value - self._difference
-                if new_floor < 6 and new_floor > 0:
-                    return [(None, [AbsoluteHint(Floor(new_floor), self._attr2 )])]
-                return []
-            else:
-                return []
-        
-        if isinstance(self._attr1, Animal):
-            if self._attr1 in all_animal_options:
-                animal_options = [self._attr1]
-            elif self._attr2 in all_animal_options:
                 floor_assignment_with_attr1 = list(filter(lambda x: self._attr1 in x, floor_assignments))
                 new_floor = floor_assignment_with_attr1[0].floor.value - self._difference
                 if new_floor < 6 and new_floor > 0:
@@ -132,7 +188,7 @@ class RelativeHint(Hint):
                     new_floor = floor.value + self._difference
                     
                     # out of bounds, is not a possibility
-                    if new_floor > 5 or new_floor < 1:
+                    if new_floor > MAX_FLOOR or new_floor < MIN_FLOOR:
                         continue
 
                     new_hint_to_satisfy = AbsoluteHint(Floor(new_floor), self._attr2)
@@ -153,20 +209,37 @@ class AbsoluteHint(Hint):
     The orange floor is the floor where the chicken lives:
         AbsoluteHint(Color.Orange, Animal.Chicken)
     """
-    def __init__(self, attr1, attr2): # Reminder: Don't change the initializer signature
+    def __init__(self, attr1: Union[Floor, Color, Animal], attr2: Union[Floor, Color, Animal]): # Reminder: Don't change the initializer signature
         self._attr1 = attr1
         self._attr2 = attr2
 
-    def check_if_satisfied(self, floor_assignments):
+    def check_if_satisfied(self, floor_assignments: List[FloorAssignment]) -> bool:
+        """
+        Checks if the hint is satisfied in the given floor assignments. 
+        Absolute hint will be sattisfied if we can find a floor assignment that has both of the hint's attributes.
+        """
         for floor_assignments in floor_assignments:
             if self._attr1 in floor_assignments and self._attr2 in floor_assignments:
                 return True
         return False
 
-    def get_possible_floor_assignments(self, empty_floors, animal_options, color_options, floor_assignments):
+    def get_possible_floor_assignments(self, 
+                                       empty_floors: List[Floor], 
+                                       all_animal_options: List[Animal], 
+                                       all_color_options: List[Color], 
+                                       floor_assignments: List[FloorAssignment]) -> List[Tuple[Optional[FloorAssignment], Optional['Hint']]]:
+        """
+        Returns a list of all posssible tuples, of floor assignments and additional hints.
+        The combination of both will satisfy the original hint to completion.
+        In Absolute hints, no additional hint will be provided (will always be None).
+        """
+        animal_options = all_animal_options
+        color_options = all_color_options
+        floor_options = empty_floors
+
         if isinstance(self._attr1, Floor):
             if self._attr1 in empty_floors:
-                empty_floors = [self._attr1]
+                floor_options = [self._attr1]
             else:
                 return []
         
@@ -184,7 +257,7 @@ class AbsoluteHint(Hint):
             
         if isinstance(self._attr2, Floor):
             if self._attr2 in empty_floors:
-                empty_floors = [self._attr2]
+                floor_options = [self._attr2]
             else:
                 return []
         
@@ -201,7 +274,7 @@ class AbsoluteHint(Hint):
                 return []
 
         possible_options = []
-        for floor in empty_floors:
+        for floor in floor_options:
             for animal in animal_options:
                 for color in color_options:
                     possible_options.append((FloorAssignment(floor=floor, color=color, animal=animal), []))
@@ -219,33 +292,63 @@ class NeighborHint(Hint):
     The yellow floor is neighboring the third floor:
         NeighborHint(Color.Yellow, Floor.Third)
     """
-    def __init__(self, attr1, attr2): # Reminder: Don't change the initializer signature
+    def __init__(self, attr1: Union[Floor, Color, Animal], attr2: Union[Floor, Color, Animal]): # Reminder: Don't change the initializer signature
         self._attr1 = attr1
         self._attr2 = attr2
+        # A NeighborHint is a directionless relative hint with distance 1. 
         self.relative_hints = [RelativeHint(attr1, attr2, 1),  RelativeHint(attr2, attr1, 1)]
 
-    def check_if_satisfied(self, floor_assignments):
+    def check_if_satisfied(self, floor_assignments: List[FloorAssignment]) -> bool:
+        """
+        Checks if the hint is satisfied in the given floor assignments. 
+        A Neighbor hint a a directitonless relative hint.
+        It will be satisfied if one of the relaive hints are satisfied.
+        """
         for relative_hint in self.relative_hints:
             if relative_hint.check_if_satisfied(floor_assignments=floor_assignments):
                 return True
         return False
 
-    def get_possible_floor_assignments(self, empty_floors, animal_options, color_options, floor_assignments):
-        return self.relative_hints[0].get_possible_floor_assignments(empty_floors, animal_options, color_options, floor_assignments) + self.relative_hints[0].get_possible_floor_assignments( empty_floors, animal_options, color_options, floor_assignments)
+    def get_possible_floor_assignments(self, 
+                                       empty_floors: List[Floor], 
+                                       all_animal_options: List[Animal], 
+                                       all_color_options: List[Color], 
+                                       floor_assignments: List[FloorAssignment]) -> List[Tuple[Optional[FloorAssignment], Optional['Hint']]]:
+        """
+        Returns a list of all posssible tuples, of floor assignments and additional hints.
+        The combination of both will satisfy the original hint to completion.
+        A NeighborHint is a direcctionless Relative hint.
+        Return all possible floor assignments of both directional hints that are neighborhint.
+        """
+        return self.relative_hints[0].get_possible_floor_assignments(empty_floors, 
+                                                                     all_animal_options, 
+                                                                     all_color_options, 
+                                                                     floor_assignments) \
+                + self.relative_hints[0].get_possible_floor_assignments(empty_floors, 
+                                                                        all_animal_options, 
+                                                                        all_color_options, 
+                                                                        floor_assignments)
 
-def remove_value(lst, value):
-    return list(filter(lambda x: x != value, lst))
+def backtrack(hints: List[Hint], 
+              floor_assignments: List[FloorAssignment], 
+              empty_floors: List[Floor], 
+              animal_options: List[Animal],
+              color_options: List[Color]) -> int:
+    """
+    Counts all possible floor assignments that satisfy the list of hints, available floors, available colors and available animals given.
+    """
 
-def backtrack(hints, floor_assignments, empty_floors, animal_options, color_options) -> int:
     if len(hints) == 0:
         return math.factorial(len(animal_options))*math.factorial(len(color_options))
     
     possible_options = 0
-    possible_floor_assignment = hints[0].get_possible_floor_assignments(empty_floors, animal_options, color_options, floor_assignments)
+
+    cur_hint = hints[0]
+    possible_floor_assignment = cur_hint.get_possible_floor_assignments(empty_floors, animal_options, color_options, floor_assignments)
 
     if len(possible_floor_assignment) == 0:
-        # Check if hint was already satisfied
-        if hints[0].check_if_satisfied(floor_assignments):
+        # Check if hint was already satisfied. If so, skip current hint.
+        if cur_hint.check_if_satisfied(floor_assignments):
             return backtrack(hints=hints[1:], 
                                       floor_assignments = list(floor_assignments),
                                       empty_floors=list(empty_floors),
@@ -254,14 +357,15 @@ def backtrack(hints, floor_assignments, empty_floors, animal_options, color_opti
 
     for floor_assigmment, additional_hints in possible_floor_assignment:
         
-        # TODO - FIX function "remove_value" so it will be generic for both cases
         if floor_assigmment is None:
+            # Hint attibute was already assigned in a previous floor, however additional hint was needed.``
             possible_options += backtrack(hints=hints[1:] + additional_hints, 
                                       floor_assignments = list(floor_assignments),
                                       empty_floors=list(empty_floors),
                                       animal_options=list(animal_options),
                                       color_options=list(color_options))
         else:
+            # With the new floor assignment, cur hint is satisfied. Remove used attributes from available options, and continue to next hint.
             possible_options += backtrack(hints=hints[1:] + additional_hints, 
                                         floor_assignments = list(floor_assignments) + [floor_assigmment],
                                         empty_floors=remove_value(empty_floors, floor_assigmment.floor),
@@ -276,12 +380,17 @@ def count_assignments(hints: List[Hint]): # Reminder: Don't change the function 
     valid assignments that satisfy these hints.
     """
 
-    # Call the backtrack function to find all valid assignments
     empty_floors = [Floor.First, Floor.Second, Floor.Third, Floor.Fourth, Floor.Fifth]
     animal_options = [Animal.Bird, Animal.Chicken, Animal.Frog, Animal.Grasshopper, Animal.Rabbit]
     color_options = [Color.Blue, Color.Green, Color.Orange, Color.Red, Color.Yellow]
+    floor_assignments = []
 
-    return backtrack(hints=hints, floor_assignments=[], empty_floors=empty_floors, animal_options=animal_options, color_options=color_options)
+    # Call the backtrack function to find all valid assignments
+    return backtrack(hints=hints,
+                     floor_assignments=floor_assignments, 
+                     empty_floors=empty_floors, 
+                     animal_options=animal_options, 
+                     color_options=color_options)
 
 
 HINTS_EX1 = [
